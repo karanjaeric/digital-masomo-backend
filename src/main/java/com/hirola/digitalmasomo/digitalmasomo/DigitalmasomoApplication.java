de98b1743cc34ea9e093db27fd1b3325921b4a8e
Erick Karanja <muthike789@gmail.com>
2020-06-13 14:40:37 +0300

    first commit

---
+++ src/main/java/com/hirola/digitalmasomo/digitalmasomo/DigitalmasomoApplication.java
@@ -0,0 +1,13 @@
+package com.hirola.digitalmasomo.digitalmasomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalmasomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalmasomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirola/digitalmasomo/digitalmasomo/DigitalmasomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirola.digitalmasomo.digitalmasomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalmasomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
